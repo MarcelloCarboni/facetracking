cdaf11633d09a049de8f96cffa15e6e3891e4e54
Marcello <marcicarboni00@gmail.com>
2022-08-05 01:23:28 +0200

    todo: add vertical control

--- track.py
+++ track.py
@@ -4,7 +4,7 @@ faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 video_capture = cv2.VideoCapture(0)
 # Value of the buffer from the center of the face 
-buffer = 5
+buffer = 30
 
 while True:
     # Capture frame-by-frame
@@ -20,19 +20,35 @@ while True:
         flags = cv2.CASCADE_SCALE_IMAGE
     )
 
+    biggest_face = (0,0,0,0)
+    if (len(faces) > 1):
+        # Find biggest rectangle
+        face_areas = []
+        for face in faces:
+            face_areas.append(face[2]*face[3])
+        print(max(face_areas), faces.index(max(face_areas)))
+        biggest_face = face_areas.index(max(face_areas))
+    elif (len(faces) == 1):
+        biggest_face = faces[0]
+    else:
+        biggest_face = (0,0,0,0)
+
+
     line_w = video_capture.get(cv2.CAP_PROP_FRAME_WIDTH)
     line_h = video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    cv2.line(frame, (int(line_w/2), 0), (int(line_w/2), int(line_h)), (255, 0, 0), 2)
 
-    # Draw a rectangle around the faces
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-        cv2.line(frame, (int(x+(w/2) - buffer), 0), (int(x+(w/2) - buffer), int(line_h)), (0, 0, 255), 4)
-        cv2.line(frame, (int(x-(w/2) - buffer), 0), (int(x-(w/2) - buffer), int(line_h)), (0, 0, 255), 4)
+    if (max(biggest_face) != 0):
+        # Draw a rectangle around the faces
+        cv2.rectangle(frame, (biggest_face[0], biggest_face[1]), (biggest_face[0]+biggest_face[2], biggest_face[1]+biggest_face[3]), (0, 255, 0), 2)
+        cv2.line(frame, (int((biggest_face[0] + (biggest_face[2]/2)) + buffer), 0), (int((biggest_face[0] + (biggest_face[2]/2)) + buffer), int(line_h)), (0, 0, 255), 2)
+        cv2.line(frame, (int((biggest_face[0] + (biggest_face[2]/2)) - buffer), 0), (int((biggest_face[0] + (biggest_face[2]/2)) - buffer), int(line_h)), (0, 0, 255), 2)
 
-    cv2.line(frame, (int(line_w/2), 0), (int(line_w/2), int(line_h)), (255, 0, 0), 4)
-
-    # Move camera
-    
+        # Move camera
+        if (int((biggest_face[0] + (biggest_face[2]/2)) - buffer) > int(line_w / 2)): # if left
+            print('left')
+        elif (int((biggest_face[0] + (biggest_face[2]/2)) + buffer) < int(line_w / 2)):
+            print('right')
 
     # Display the resulting frame
     cv2.imshow('Video', frame)
