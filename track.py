9227c49237f30daa3295fcabb46b29ab3597d1f1
Marcello <marcicarboni00@gmail.com>
2022-08-05 00:33:15 +0200

    added buffer

--- track.py
+++ track.py
@@ -3,6 +3,8 @@ import numpy as np
 faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 video_capture = cv2.VideoCapture(0)
+# Value of the buffer from the center of the face 
+buffer = 5
 
 while True:
     # Capture frame-by-frame
@@ -18,12 +20,15 @@ while True:
         flags = cv2.CASCADE_SCALE_IMAGE
     )
 
+    line_w = video_capture.get(cv2.CAP_PROP_FRAME_WIDTH)
+    line_h = video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
     # Draw a rectangle around the faces
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        cv2.line(frame, (int(x+(w/2) - buffer), 0), (int(x+(w/2) - buffer), int(line_h)), (0, 0, 255), 4)
+        cv2.line(frame, (int(x-(w/2) - buffer), 0), (int(x-(w/2) - buffer), int(line_h)), (0, 0, 255), 4)
 
-    line_w = video_capture.get(cv2.CAP_PROP_FRAME_WIDTH)
-    line_h = video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT)
     cv2.line(frame, (int(line_w/2), 0), (int(line_w/2), int(line_h)), (255, 0, 0), 4)
 
     # Move camera
